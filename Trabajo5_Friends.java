1815f52cdc4a4e9cf1eb673c2139cbc9abe48017
ycanasq <71304352+ycanasq@users.noreply.github.com>
2025-11-19 13:45:44 -0500

    Add files via upload

---
+++ Trabajo5_Friends.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajo5_friends;
+
+/**
+ *
+ * @author MacBook
+ */
+public class Trabajo5_Friends {
+
+    public static void main(String[] args) {
+        Principal formulario = new Principal();
+        formulario.setVisible(true);
+    }
+}
